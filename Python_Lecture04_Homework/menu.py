51f24a6c20ab9c513899c881cc3560f68f6edf1d
DenLysko <denislysko@yandex.ru>
2023-10-07 12:06:11 +0300

    refactor: Переписал if-else на match и вынес

--- Python_Lecture04_Homework/menu.py
+++ Python_Lecture04_Homework/menu.py
@@ -1,47 +1,44 @@
-import csv
 import methods_for_options
-
-
-def get_reader():
-    corp_summay_csv = open("Corp_Summary.csv", "r", encoding="utf-8")
-    my_reader = csv.reader(corp_summay_csv)
-    return my_reader
-
-
-def write_report(report):
-    report__csv = open("report.csv", "w", newline="")
-    writer = csv.writer(report__csv)
-    writer.writerows(report.items())
 import writer_and_reader
 
 
 def step1() -> None:
-    my_reader = get_reader()
+    my_reader, option = get_option_and_reader()
+
+    match option:
+        case 1:
+            departments_and_commands = methods_for_options.generate_command_hierarchy(
+                my_reader
+            )
+            for key, value in departments_and_commands.items():
+                print("{0}: {1}.".format(key, ", ".join(list(value))))
+        case 2:
+            report = methods_for_options.generate_report(my_reader)
+            for key, value in report.items():
+                # Не было указаний до какого числа необходимо огруглять среднюю зарплату,
+                # поэтому вывожу число полностью. Но всегда можно написать round(value[4], 2), например
+                print(
+                    f'Департамент "{key}". Численность: {value[0]}, минимальная зарплата: {value[2]}, '
+                    f"максимальная зарплата: {value[3]}, средняя зарплата: {value[4]}"
+                )
+
+        case 3:
+            report = methods_for_options.generate_report(my_reader)
+            writer_and_reader.write_report(report)
+
+
+def get_option_and_reader():
+    my_reader = writer_and_reader.get_reader()
     print(
-        "Выберите пункт меню: \n 1. Вывести иерархию команд \n 2. Вывести сводный отчёт по департаментам \n 3. Cохранить сводный отчёт в виде csv-файла"
+        "Выберите пункт меню: \n 1. Вывести иерархию команд \n"
+        + "2. Вывести сводный отчёт по департаментам \n 3. Cохранить сводный отчёт в виде csv-файла"
     )
 
     option = 0
     options = [1, 2, 3]
     while option not in options:
         option = int(input())
-
-    if option == 1:
-        departments_and_commands = methods_for_options.generate_command_hierarchy(
-            my_reader
-        )
-        for key, value in departments_and_commands.items():
-            print("{0}: {1}.".format(key, ", ".join(list(value))))
-    elif option == 2:
-        report = methods_for_options.generate_report(my_reader)
-        for key, value in report.items():
-            print(
-                f'Департамент "{key}". Численность: {value[0]}, минимальная зарплата: {value[2]}, '
-                f"максимальная зарплата: {value[3]}, средняя зарплата: {value[4]}"
-            )
-    elif option == 3:
-        report = methods_for_options.generate_report(my_reader)
-        write_report(report)
+    return my_reader, option
 
 
 if __name__ == "__main__":
